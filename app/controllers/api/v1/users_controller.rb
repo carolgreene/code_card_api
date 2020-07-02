183019fb4602fd3197761ac26094bf7564280136
carolgreene <carolgreene3@gmail.com>
2020-07-01 18:57:39 -0700

    added show & create methods

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,29 @@ class Api::V1::UsersController < ApplicationController
     users = User.all 
     render json: UserSerializer.new(users)
   end
+
+  def show 
+    user = User.find(params[:id])
+    render json: UserSerializer.new(user)
+  end
+
+  def create 
+    user = User.new(user_params)
+    if user.save 
+      render json: UserSerializer.new(user) 
+    else 
+      error_resp = {
+        error: user.errors.full_messages.to_sentence
+      }
+      render json: error_resp, status: :unprocessable_entity
+    end 
+  end
+
+  private 
+
+  def user_params 
+    params.require(:user).permit(:name, :password)
+  end
+
+
 end
