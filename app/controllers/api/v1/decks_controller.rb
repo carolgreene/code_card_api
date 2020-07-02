643e59fb9f12e14f4ecec9cb5fde6c484f4c39f0
carolgreene <carolgreene3@gmail.com>
2020-07-01 18:47:18 -0700

    added show & create methods

--- app/controllers/api/v1/decks_controller.rb
+++ app/controllers/api/v1/decks_controller.rb
@@ -4,5 +4,30 @@ class Api::V1::DecksController < ApplicationController
     decks = Deck.all 
     render json: DeckSerializer.new(decks)
   end
+
+  def show 
+    deck = Deck.find(params[:id])
+    render json: DeckSerializer.new(deck)
+  end
+
+  def create 
+    deck = Deck.new(deck_params)
+
+    if deck.save 
+      render json: DeckSerializer.new(deck)
+    else 
+      error_resp = {
+        error: deck.errors.full_messages.to_sentence
+      }
+      render json: error_resp, status: :unprocessable_entity
+    end
+  end
+
+  private 
+
+  def deck_params 
+    params.require(:deck).permit(:name, :user_id)
+  end
+
   
 end
