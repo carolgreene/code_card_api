1ac6ffd94fdab35726f2ee3cc60c190fd3c03ce0
carolgreene <carolgreene3@gmail.com>
2020-07-04 14:28:23 -0700

    update, destroy, & set_card

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::CardsController < ApplicationController
+  before_action :set_card, only: [:show, :update, :destroy]
 
   def index 
     cards = Card.all 
@@ -23,11 +24,32 @@ class Api::V1::CardsController < ApplicationController
     end
   end
 
+  def update 
+    if card.update(card_params) 
+      render json: CardSerializer.new(card)
+    else
+      error_resp = {
+        error: card.errors.full_messages.to_sentence
+      }
+      render json: error_resp, status: :unprocessable_entity
+    end 
+  end
+
+  def destroy
+    cards = Card.all
+    card.destroy
+    render json: CardSerializer.new(cards) 
+  end
+
+
     private 
+
+    def set_card 
+      card = Card.find(params[:id])
+    end
 
     def card_params 
       params.require(:card).permit(:front, :back, deck_id)
     end
-
 
 end
