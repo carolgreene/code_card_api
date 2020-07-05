6a55c9e51c644f8f91c36e05e70530368eb3e961
carolgreene <carolgreene3@gmail.com>
2020-07-05 07:55:52 -0700

    update, destroy, & set_user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
 
   def index 
     users = User.all 
@@ -22,7 +23,28 @@ class Api::V1::UsersController < ApplicationController
     end 
   end
 
+  def update
+    if user.update(user_params)
+      render json: UserSerializer.new(user)
+    else
+      error_resp = {
+        error: user.errors.full_messages.to_sentence
+      }
+      render json: error_resp, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    users = User.all
+    user.destroy
+    render json: UserSerializer.new(users)
+  end
+
   private 
+
+  def set_user 
+    user = User.find(params[:id])
+  end
 
   def user_params 
     params.require(:user).permit(:name, :password)
