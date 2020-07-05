fa42ec96fe52b17c97941e6cf2f0fc5aa02fc44c
carolgreene <carolgreene3@gmail.com>
2020-07-05 07:51:10 -0700

    update, destroy, & set_deck

--- app/controllers/api/v1/decks_controller.rb
+++ app/controllers/api/v1/decks_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::DecksController < ApplicationController
+  before_action :set_deck, only: [:show, :update, :destroy]
 
   def index
     decks = Deck.all 
@@ -23,7 +24,30 @@ class Api::V1::DecksController < ApplicationController
     end
   end
 
+  def update
+    if deck.update(deck_params)
+      render json: DeckSerializer.new(deck)
+    else
+      error_resp = {
+        error: deck.errors.full_messages.to_sentence
+      }
+      render json: error_resp, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    decks = Deck.all
+    deck.destroy
+    render json: DeckSerializer.new(decks)
+  end
+
+
+
   private 
+
+  def set_deck 
+    deck = Deck.find(params[:id])
+  end
 
   def deck_params 
     params.require(:deck).permit(:name, :user_id)
