4752e89fb918c40a89bda3faf37d1f6abd18ab10
carolgreene <carolgreene3@gmail.com>
2020-07-09 19:07:50 -0700

    fetch decks

--- code-card-frontend/src/index.js
+++ code-card-frontend/src/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   fetchCards()
+  fetchDecks()
 })
 
 function fetchCards() {
@@ -11,6 +12,15 @@ function fetchCards() {
     console.log(results)
   })
 
+}
 
+function fetchDecks() {
+  let decks = document.getElementById("decks")
+  let li = document.createElement("li")
+  fetch(`http://10.0.0.99:3000/api/v1/decks`)
+  .then((res) => res.json())
+  .then(results => {
+    console.log(results)
+  })
 
 }
