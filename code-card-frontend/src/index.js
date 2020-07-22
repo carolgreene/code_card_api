c1466ed4685a471da53503590b53a7e9a4cf8e8e
carolgreene <carolgreene3@gmail.com>
2020-07-22 16:11:38 -0700

    added basic display for decks

--- code-card-frontend/src/index.js
+++ code-card-frontend/src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   //fetchCards()
-  //fetchDecks()
-  fetchUsers()
+  fetchDecks()
+  //fetchUsers()
 })
 /*
 function fetchCards() {
@@ -12,19 +12,51 @@ function fetchCards() {
   .then(results => {
     console.log(results)
   })
-
 }
+*/
 
 function fetchDecks() {
-  let decks = document.getElementById("decks")
+  let decks = document.getElementById("deckUl")
   let li = document.createElement("li")
   fetch(`http://10.0.0.99:3000/api/v1/decks`)
   .then((res) => res.json())
   .then(results => {
-    console.log(results)
+    //console.log(results.data)
+    results.data.forEach(deck => {
+      //console.log(deck.attributes.name)
+      displayDecks(deck)
+    })
   })
-  */
+}
 
+    function displayDecks(deck) {
+      let ul = document.getElementById("deckUl")
+      let li = document.createElement('li')
+      li.innerText = deck.attributes.name
+      ul.appendChild(li)
+
+    }
+
+
+    /*
+    //need to work out how to get full list. only getting last one
+    //follow this pattern
+    var names = ['John', 'Jane'];
+var ul = document.getElementById("friendsList");
+
+for (var i = 0; i < names.length; i++) {
+    var name = names[i];
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(name));
+    ul.appendChild(li);
+}
+
+*/
+  
+
+
+  
+  /*
   function fetchUsers() {
   let cards = document.getElementById("users")
   let li = document.createElement("li")
@@ -32,7 +64,8 @@ function fetchDecks() {
   .then((res) => res.json())
   .then(results => {
     console.log(results)
-  })
+  }) 
 
 }
+*/
 
