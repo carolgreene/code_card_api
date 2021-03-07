7b26825d2af63f400a95e7ce48e08b4671887afd
carolgreene <carolgreene3@gmail.com>
2021-03-07 10:41:31 -0800

    nested cards & changed back

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users 
       resources :decks
       resources :cards
       post '/login', to: 'auth#create'
